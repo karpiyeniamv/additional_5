a54de6e8bfa42c0d0444a0e491fad543175db4a8
Maryia Karpiyenia <karpiyenia.mv@gmail.com>
2018-09-28 23:02:10 +0300

    res

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    var stack = [];
+    var count = 0;
+    if (str.length%2==1)
+        return false;
+    for (i = 0; i < str.length; i++) {
+        var j = 0;
+        if (str[i] == '|')
+        {
+
+            if (count==0)
+            {
+                count++;
+                stack.push(str[i]);
+            }
+            else
+            if (count==1)
+            {
+                if  (stack.pop() != '|')
+                    return false;
+                else
+                    count--;
+            }
+        }
+        else {
+
+            for (j; j < bracketsConfig.length; j++) {
+                if (str[i] == bracketsConfig[j][0]) {
+                    stack.push(str[i]);
+                }
+                else if (str[i] == bracketsConfig[j][1]) {
+                    if (stack.pop() != bracketsConfig[j][0]) {
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+    if (stack.length == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+};
